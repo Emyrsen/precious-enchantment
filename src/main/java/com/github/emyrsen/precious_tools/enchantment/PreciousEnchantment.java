fb65ad4ed9dd88c910bb417627c60627cff5afd2
Jared Jones <jared@jaredcj.com>
2022-04-14 22:40:23 -0600

    Exclude the enchantment from wearables

--- src/main/java/com/github/emyrsen/precious_tools/enchantment/PreciousEnchantment.java
+++ src/main/java/com/github/emyrsen/precious_tools/enchantment/PreciousEnchantment.java
@@ -3,6 +3,9 @@ package com.github.emyrsen.precious_tools.enchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Wearable;
 
 public class PreciousEnchantment extends Enchantment {
 
@@ -19,5 +22,11 @@ public class PreciousEnchantment extends Enchantment {
   @Override
   public int getMinPower(int level) {
     return 1;
+  }
+
+  @Override
+  public boolean isAcceptableItem(ItemStack stack) {
+    Item item = stack.getItem();
+    return super.isAcceptableItem(stack) && !(item instanceof Wearable);
   }
 }
